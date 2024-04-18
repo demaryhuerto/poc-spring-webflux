504551cb3cc735b88d76cb2e848cf818dc2d3866
Gabriel Jiménez Peña <gjimenez@nextdigital.es>
2024-04-18 14:58:37 +0200

    initial spring webflux application

---
+++ src/main/java/es/nextdigital/training/spring/pocwebflux/PocWebfluxApplication.java
@@ -0,0 +1,13 @@
+package es.nextdigital.training.spring.pocwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/nextdigital/training/spring/pocwebflux/PocWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package es.nextdigital.training.spring.pocwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
